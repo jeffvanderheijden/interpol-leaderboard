a67bb82214e8061db953fcd816d50a584f51c47a
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-07-09 19:18:15 +0200

    Trying to time one globe rotation to a data update so it doesn't rerender and screen tear

--- src/components/ConnectedBots/ConnectedBots.js
+++ src/components/ConnectedBots/ConnectedBots.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react"
+// import useInterval from "./../../helpers/hooks/useInterval"
+import { getGroups } from "./../../assets/data/dataLayer"
 import "./ConnectedBots.css"
 
 const ConnectedBots = () => {
