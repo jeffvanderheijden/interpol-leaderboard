916a28856c0db8cccc72ce307861e904f902302f
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-07-09 20:19:57 +0200

    Changed fonts correctly

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -10,26 +10,26 @@ const Leaderboard = () => {
                     <div className="avatar">
                         <span>3</span>
                     </div>
-                    <div className="teamName">De Hakkende Hekkers</div>
+                    <h1 className="teamName">De Hakkende Hekkers</h1>
                     <div className="teamClass">D1A</div>
-                    <div className="score">2000</div>
+                    <h1 className="score">2000</h1>
                 </div>
                 <div className="podium first">
                     <div className="avatar">
                         <img className="crown" src={Crown} alt={'Leader'} />
                         <span>1</span>
                     </div>
-                    <div className="teamName">Team Victory</div>
+                    <h1 className="teamName">Team Victory</h1>
                     <div className="teamClass">D1C</div>
-                    <div className="score">2450</div>
+                    <h1 className="score">2450</h1>
                 </div>
                 <div className="podium second">
                     <div className="avatar">
                         <span>2</span>
                     </div>
-                    <div className="teamName">D1F de gekste</div>
+                    <h1 className="teamName">D1F de gekste</h1>
                     <div className="teamClass">D1F</div>
-                    <div className="score">2150</div>
+                    <h1 className="score">2150</h1>
                 </div>
             </div>
         </div>
