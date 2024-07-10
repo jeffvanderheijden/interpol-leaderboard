ff11ed058b757690bd4335b15a798c3df0cf2486
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-07-10 07:15:51 +0200

    Added first, second and third teams to leaderboard

--- src/components/Leaderboard/Leaderboard.js
+++ src/components/Leaderboard/Leaderboard.js
@@ -1,43 +1,63 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import Crown from "./../../assets/images/crown.svg";
 import "./Leaderboard.css"
 
 const Leaderboard = ({
     topThreeGroups
 }) => {
+    const [highestPoints, setHighestPoints] = useState({});
+    const [secondHighestPoints, setSecondHighestPoints] = useState({});
+    const [lowestPoints, setLowestPoints] = useState({});
 
     useEffect(() => {
-        console.log(topThreeGroups);
+        if (topThreeGroups.length > 0) {
+            // Sort the array based on the points in descending order
+            const sortedData = topThreeGroups.slice().sort((a, b) => parseInt(b.points) - parseInt(a.points));
+
+            const highestPoints = sortedData[0];
+            const secondHighestPoints = sortedData[1];
+            const lowestPoints = sortedData[sortedData.length - 1];
+
+            setHighestPoints(highestPoints);
+            setSecondHighestPoints(secondHighestPoints);
+            setLowestPoints(lowestPoints);
+        }
     }, [topThreeGroups]);
 
     return (
         <div className="leaderBoard">
             <div className="leaderStage">
-                <div className="podium third">
-                    <div className="avatar">
-                        <span>3</span>
+                {lowestPoints && (
+                    <div className="podium third">
+                        <div className="avatar">
+                            <span>3</span>
+                        </div>
+                        <h1 className="teamName">{lowestPoints.name}</h1>
+                        <div className="teamClass">{lowestPoints.class}</div>
+                        <h1 className="score">{lowestPoints.points}</h1>
                     </div>
-                    <h1 className="teamName">De Hakkende Hekkers</h1>
-                    <div className="teamClass">D1A</div>
-                    <h1 className="score">2000</h1>
-                </div>
-                <div className="podium first">
-                    <div className="avatar">
-                        <img className="crown" src={Crown} alt={'Leader'} />
-                        <span>1</span>
+                )}
+                {highestPoints && (
+                    <div className="podium first">
+                        <div className="avatar">
+                            <img className="crown" src={Crown} alt={'Leader'} />
+                            <span>1</span>
+                        </div>
+                        <h1 className="teamName">{highestPoints.name}</h1>
+                        <div className="teamClass">{highestPoints.class}</div>
+                        <h1 className="score">{highestPoints.points}</h1>
                     </div>
-                    <h1 className="teamName">Team Victory</h1>
-                    <div className="teamClass">D1C</div>
-                    <h1 className="score">2450</h1>
-                </div>
-                <div className="podium second">
-                    <div className="avatar">
-                        <span>2</span>
+                )}
+                {secondHighestPoints && (
+                    <div className="podium second">
+                        <div className="avatar">
+                            <span>2</span>
+                        </div>
+                        <h1 className="teamName">{secondHighestPoints.name}</h1>
+                        <div className="teamClass">{highestPoints.class}</div>
+                        <h1 className="score">{highestPoints.points}</h1>
                     </div>
-                    <h1 className="teamName">D1F de gekste</h1>
-                    <div className="teamClass">D1F</div>
-                    <h1 className="score">2150</h1>
-                </div>
+                )}
             </div>
         </div>
     )
